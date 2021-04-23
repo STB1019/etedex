baf065d95ad3873599c14b7f1913fff1a55ad1f2
Enrico Brambilla <enric.brambilla@gmail.com>
2021-04-23 10:47:31 +0200

    refactor

--- client_secure.py
+++ client_secure.py
@@ -1,22 +1,24 @@
 #! /bin/env python3
+
 import tinyec.ec
 from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
 from tinyec import registry
 import secrets
 import socket
 from threading import Thread
-from Crypto.Cipher import AES
 import argparse
 
 IP = "178.128.200.134"
 PORT = 6073
 
+
 class SendThread(Thread):
-    def __init__(self, conn, secret, iv):
+    def __init__(self, conn, aes_secret, iv):
         Thread.__init__(self)
         self.conn = conn
-        self.secret = secret
-        self.cipher = AES.new(secret, AES.MODE_CBC, iv)
+        self.secret = aes_secret
+        self.cipher = AES.new(aes_secret, AES.MODE_CBC, iv)
 
     def run(self):
         while True:
@@ -24,11 +26,11 @@ class SendThread(Thread):
 
 
 class ReceiveThread(Thread):
-    def __init__(self, conn, secret, iv):
+    def __init__(self, conn, aes_secret, iv):
         Thread.__init__(self)
         self.conn = conn
-        self.secret = secret
-        self.cipher = AES.new(secret, AES.MODE_CBC, iv)
+        self.secret = aes_secret
+        self.cipher = AES.new(aes_secret, AES.MODE_CBC, iv)
 
     def run(self):
         while True:
@@ -41,40 +43,38 @@ def compress(pub_key):
     return hex(pub_key.x) + hex(pub_key.y % 2)[2:]
 
 
+if __name__ == "__main__":
 
-parser = argparse.ArgumentParser(description='etedex')
-parser.add_argument("-i", "--ip", dest='ip',
-                    help="specify the ip", type=str)
+    parser = argparse.ArgumentParser(description='etedex')
+    parser.add_argument("-i", "--ip", dest='ip',
+                        help="specify the ip", type=str)
 
-parser.add_argument("-p", dest='psw',
-                    help="specify the password", type=int)
+    parser.add_argument("-p", dest='psw',
+                        help="specify the password", type=int)
 
-args = parser.parse_args()
-ip = args.ip
-psw = args.psw
-if ip: IP = ip
+    args = parser.parse_args()
+    ip = args.ip
+    psw = args.psw
+    if ip: IP = ip
 
+    curve = registry.get_curve('brainpoolP256r1')
+    mypriv = secrets.randbelow(curve.field.n)
+    print("mypub ", compress(mypriv * curve.g))
 
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((IP, PORT))
+    client.send(psw.to_bytes(32, byteorder='big'))
+    client.send(psw.to_bytes(32, byteorder='big'))
 
-curve = registry.get_curve('brainpoolP256r1')
-mypriv = secrets.randbelow(curve.field.n)
-print("mypub ", compress(mypriv * curve.g))
+    client.send((mypriv * curve.g).x.to_bytes(32, byteorder='big'))
+    client.send((mypriv * curve.g).y.to_bytes(32, byteorder='big'))
 
+    othpubx = client.recv(32)
+    othpuby = client.recv(32)
+    othpub = tinyec.ec.Point(curve, int.from_bytes(othpubx, byteorder='big'), int.from_bytes(othpuby, byteorder='big'))
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((IP, PORT))
-client.send(psw.to_bytes(32, byteorder='big'))
-client.send(psw.to_bytes(32, byteorder='big'))
+    print("othpub ", compress(othpub))
+    secret = mypriv * othpub
 
-client.send((mypriv * curve.g).x.to_bytes(32, byteorder='big'))
-client.send((mypriv * curve.g).y.to_bytes(32, byteorder='big'))
-
-othpubx = client.recv(32)
-othpuby = client.recv(32)
-othpub = tinyec.ec.Point(curve, int.from_bytes(othpubx, byteorder='big'), int.from_bytes(othpuby, byteorder='big'))
-
-print("othpub ", compress(othpub))
-secret = mypriv * othpub
-
-SendThread(client, secret.x.to_bytes(32, byteorder='big'), secret.y.to_bytes(32, byteorder='big')[:16]).start()
-ReceiveThread(client, secret.x.to_bytes(32, byteorder='big'), secret.y.to_bytes(32, byteorder='big')[:16]).start()
+    SendThread(client, secret.x.to_bytes(32, byteorder='big'), secret.y.to_bytes(32, byteorder='big')[:16]).start()
+    ReceiveThread(client, secret.x.to_bytes(32, byteorder='big'), secret.y.to_bytes(32, byteorder='big')[:16]).start()
--- server_secure.py
+++ server_secure.py
@@ -1,10 +1,15 @@
 #! /bin/env python3
+
 import socket
 import argparse
 from threading import Thread
 from collections import defaultdict
 
+IP = '0.0.0.0'
+PORT = 6073
+
 connected_users = defaultdict(int) 
+
 
 class ConnectionProxy(Thread):
 	def __init__(self, conns: tuple, addrs: tuple):
@@ -13,33 +18,34 @@ class ConnectionProxy(Thread):
 		self.addrs = addrs
 
 	def run(self):
-		print(f"{self.conns[0]} connected to {self.conns[1]}") #from 0 to 1 // outbound
-		#from_client = b""
+		print(f"{self.conns[0]} connected to {self.conns[1]}")  # from 0 to 1 // outbound
+		# from_client = b""
 		while True:
 			data = self.conns[0].recv(4096)
 			print(data)
 			self.conns[1].send(data)
 			if not data: break
-			#from_client += data
-			#print(from_client.decode())
+			# from_client += data
+			# print(from_client.decode())
 			
 
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('0.0.0.0', 6073))
-serv.listen(1000)
+if __name__ == "__main__":
 
+	serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serv.bind((IP, PORT))
+	serv.listen()
 
-while True:
-	conn, addr = serv.accept()
-	hash_mine = conn.recv(32)
-	print(hash_mine)
-	hash_wanted = conn.recv(32)
-	print(hash_wanted)
-	if hash_wanted not in connected_users:
-		connected_users[hash_mine] = (conn, addr, hash_wanted)
-	else:
-		(c, a, h) = connected_users[hash_wanted]
-		if h == hash_mine:
-			connected_users.pop(hash_wanted)
-			ConnectionProxy((conn, c), (addr, a)).start()
-			ConnectionProxy((c, conn), (a, addr)).start()
+	while True:
+		conn, addr = serv.accept()
+		hash_mine = conn.recv(32)
+		print(hash_mine)
+		hash_wanted = conn.recv(32)
+		print(hash_wanted)
+		if hash_wanted not in connected_users:
+			connected_users[hash_mine] = (conn, addr, hash_wanted)
+		else:
+			(c, a, h) = connected_users[hash_wanted]
+			if h == hash_mine:
+				connected_users.pop(hash_wanted)
+				ConnectionProxy((conn, c), (addr, a)).start()
+				ConnectionProxy((c, conn), (a, addr)).start()
